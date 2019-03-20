b505f38d1130d49c39e272eb8c4a9898edcaa9cc
Maximillian Lowe <maxjlowe@gmail.com>
2019-03-20 09:56:18 -0500

    sec 7 lec 60 complete

--- app.js
+++ app.js
@@ -1,69 +1,34 @@
-// Keys and values should be wrapped in " " in customer.json for valid JSON syntax unless it's a number  SEE JSONLINT.com for syntactical validation
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
-document.getElementById('button1').addEventListener
-('click', loadCustomer);
+function getJokes(e) {
+  //console.log('get jokes');
+  const number = document.querySelector('input[type="number"]').value;
 
-document.getElementById('button2').addEventListener
-('click', loadCustomers);
-
-
-// Load Single Customer
-function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'customer.json', true);
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`,
+  true);
 
-  xhr.onload = function(){
+  xhr.onload = function() {
     if(this.status === 200) {
-      //console.log(this.responseText);
-    
-      const customer = JSON.parse(this.responseText);
-
-      const output = `
-      <ul>
-        <li>ID: ${customer.id}</li>
-        <li>Name: ${customer.name}</li>
-        <li>Company: ${customer.company}</li>
-        <li>Phone: ${customer.phone}</li>
-      </ul>
-      `;
-
-      document.getElementById('customer').innerHTML = output;
-    }
-  }
-
-  xhr.send();
-}
-
-// Load Customers
-function loadCustomers(e) {
-  const xhr = new XMLHttpRequest();
-
-  xhr.open('GET', 'customers.json', true);
-
-  xhr.onload = function(){
-    if(this.status === 200) {
-      //console.log(this.responseText);
-    
-      const customers = JSON.parse(this.responseText);
+      const response = JSON.parse(this.responseText);
+      console.log(response);
 
       let output = '';
 
-      customers.forEach(function(customer){
-        output += `
-        
-      <ul>
-        <li>ID: ${customer.id}</li>
-        <li>Name: ${customer.name}</li>
-        <li>Company: ${customer.company}</li>
-        <li>Phone: ${customer.phone}</li>
-      </ul>
-      `;
-      });
-      document.getElementById('customers').innerHTML = output;
-      
+      if(response.type === 'success') {
+        response.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += '<li>Something went wrong</li>';
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
     }
   }
 
   xhr.send();
+
+  e.preventDefault();
 }
