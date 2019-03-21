72bce0a8445007d88a908222e59794f822c2c675
Maximillian Lowe <maxjlowe@gmail.com>
2019-03-20 19:35:11 -0500

    Sec 7 Lec 62 Complete

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
-document.querySelector('.get-jokes').addEventListener('click', getJokes);
+const posts = [
+  {title: 'Post One', body: 'This is post one'},
+  {title: 'Post Two', body: 'This is post two'}
+];
 
-function getJokes(e) {
-  //console.log('get jokes');
-  const number = document.querySelector('input[type="number"]').value;
+// function createPost(post) {
+//   setTimeout(function() {
+//     posts.push(post);
+//   }, 2000);
+// }
 
-  const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`,
-  true);
+// function getPosts() {
+//   setTimeout(function() {
+//     let output = '';
+//     posts.forEach(function(post){
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
 
-  xhr.onload = function() {
-    if(this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
+// createPost({title: 'Post Three', body: 'This is post three'});
 
-      let output = '';
+// getPosts();
 
-      if(response.type === 'success') {
-        response.value.forEach(function(joke){
-          output += `<li>${joke.joke}</li>`;
-        });
-      } else {
-        output += '<li>Something went wrong</li>';
-      }
-
-      document.querySelector('.jokes').innerHTML = output;
-    }
-  }
-
-  xhr.send();
-
-  e.preventDefault();
+function createPost(post, callback) {
+  setTimeout(function() {
+    posts.push(post);
+    callback();
+  }, 2000);
 }
+
+
+function getPosts() {
+  setTimeout(function() {
+    let output = '';
+    posts.forEach(function(post){
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
